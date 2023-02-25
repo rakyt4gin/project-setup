444a40b3eba35d913a77631f0145a0b62511170d
Andrei Skrinnik <81526462+rakyt4gin@users.noreply.github.com>
2023-02-25 23:31:08 +0300

    setup tailwind

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from 'src/data/routes';
+import styles from './Main.module.css';
 
 const MainPage = () => {
   const navigate = useNavigate();
   return (
-    <button onClick={() => navigate(ROUTES.SIGN_IN)}>Go to Sign In Page</button>
+    <button className={styles.btn} onClick={() => navigate(ROUTES.SIGN_IN)}>
+      Go to Sign In Page
+    </button>
   );
 };
 
