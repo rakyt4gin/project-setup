d8db7c3976df48c6f9f13a8ae3cb1fc8414a30df
Andrei Skrinnik <81526462+rakyt4gin@users.noreply.github.com>
2023-03-02 13:25:56 +0300

    setup svgr

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,13 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from 'src/data/routes';
 import Button from '@mui/material/Button';
+import { ReactComponent as TestIcon } from '../../assets/svg/test.svg';
 
 const MainPage = () => {
   const navigate = useNavigate();
   return (
-    <Button variant="contained" onClick={() => navigate(ROUTES.SIGN_IN)}>
-      Go to Sign In Page
-    </Button>
+    <>
+      <TestIcon width={35} />
+      <Button variant="contained" onClick={() => navigate(ROUTES.SIGN_IN)}>
+        Go to Sign In Page
+      </Button>
+    </>
   );
 };
 
