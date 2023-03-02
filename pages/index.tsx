11506db71c36cb4755666b7193041a1e371a240d
Andrei Skrinnik <81526462+rakyt4gin@users.noreply.github.com>
2023-03-02 14:24:21 +0300

    setup alias

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { TestComponent } from "components/Test";
 import NextSvg from "public/thirteen.svg";
 
 export default function Home() {
     return (
         <div>
             <NextSvg />
+            <TestComponent />
             <button className="border p-3">Submit</button>
         </div>
     );
---
+++ src/components/Test/Test.tsx
@@ -0,0 +1,5 @@
+const TestComponent = () => {
+    return <div>test</div>;
+};
+
+export default TestComponent;
